47222a0735ee4f3e64f05bc91c88fe3a377e85ba
Emiriopriimo Nadyzar Baruna <113841194+NadyzarB@users.noreply.github.com>
2022-09-18 20:53:22 +0700

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,13 @@
+#fungsi capitalize, Huruf pertama input dibesarkan dan sisanya dikecilkan
+#pada elif, H kapital karena huruf pertama pasti kapital
+
+x= input("Greetings? ").capitalize()
+
+if "Hello" in x:
+    print("$0")
+elif ("H") in x:
+    print("$20")
+else:
+    print("$100")
+
+#sekian terimakaji
